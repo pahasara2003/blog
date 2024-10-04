3916c82f4ca6ad0907fb350397be590a0d13eb7b
pahasara wickramasinghe <138836186+pahasara2003@users.noreply.github.com>
2024-10-04 22:04:22 +0530

    article design update

--- app/articles/[title]/page.tsx
+++ app/articles/[title]/page.tsx
@@ -3,6 +3,8 @@ import BlogHeader from "@/components/BlogHeader";
 import { PrismaClient } from "prisma/prisma-client";
 import "../../blog.css";
 import ArticlePageRetatedPosts from "@/components/ArticlePageRelatedPosts";
+import type { Metadata } from "next";
+
 const prisma = new PrismaClient();
 
 const removeStyles = (html: string) => {
@@ -14,6 +16,22 @@ const removeStyles = (html: string) => {
 
   return html;
 };
+
+export async function generateMetadata(Url: any): Promise<Metadata> {
+  // read route params
+
+  // fetch data
+  const data = await prisma.posts.findFirst({
+    where: { slug: decodeURIComponent(Url.params.title) },
+  });
+
+  prisma.$disconnect();
+
+  return {
+    title: data?.title,
+  };
+}
+
 const Page = async (Url: any) => {
   const data = await prisma.posts.findFirst({
     where: { slug: decodeURIComponent(Url.params.title) },
@@ -34,10 +52,10 @@ const Page = async (Url: any) => {
     <div className="flex  gap-5 justify-center ">
       <div className="bg-white m-5 md:mx-[10vw] lg:mx-[0vw] mx-0">
         <BlogHeader data={data} date={date} />
-        <div
+        {/* <div
           dangerouslySetInnerHTML={{ __html: cleanHTML }}
           className=" text-[1rem] xl:px-10 font-serif overflow-x-clip max-w-[75vw] px-3 m-auto "
-        ></div>
+        ></div> */}
       </div>
       <ArticlePageRetatedPosts />
     </div>
